cfcf8bc3fc7aba9e5ab5580403a078d75677c1af
KPATOC33 <69507581+KPATOC33@users.noreply.github.com>
2020-11-07 17:15:52 +0200

    добавил 3 тест

--- src/Day3/Switch.java
+++ src/Day3/Switch.java
@@ -1,4 +1,58 @@
 package Day3;
 
+import java.util.Scanner;
+
 public class Switch {
+    public static void main(String[] args) {
+        //создаем обьект типа Scanner и добавляем входной поток System.in
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи возраст ");
+        //nextInt метод принемающий числовые данные
+        //nextLine метод принемающий числовые данные
+        String age  = scanner.nextLine();
+        switch(age) {
+            case "Ноль" :
+                System.out.println("Ты родился ");
+                break;
+            case "Семь" :
+                System.out.println("Ты пошел в школу ");
+                break;
+            case "Восемнадцать" :
+                System.out.println("Ты закончил школу ");
+                break;
+                //default ставят для обозначения того что не одно из условий не сработало
+            default:
+                System.out.println("Ни одно из условий не сработало ");
+        }
+    }
 }
+
+
+
+//package Day3;
+//
+//        import java.util.Scanner;
+//
+//public class Switch {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введи возраст ");
+//        //nextInt метод принемающий числовые данные
+//         //nextLine метод принемающий числовые данные
+//        int age  = scanner.nextInt();
+//        switch(age) {
+//            case 0 :
+//                System.out.println("Ты родился ");
+//                break;
+//            case 7 :
+//                System.out.println("Ты пошел в школу ");
+//                break;
+//            case 18 :
+//                System.out.println("Ты закончил школу ");
+//                break;
+//            //default ставят для обозначения того что не одно из условий не сработало
+//            default:
+//                System.out.println("Ни одно из условий не сработало ");
+//        }
+//    }
+//}
--- src/Day3/break_continue.java
+++ src/Day3/break_continue.java
@@ -1,4 +1,31 @@
 package Day3;
-
+//это выдает все нечетные числа до 15
 public class break_continue {
+    public static void main(String[] args) {
+for (int i = 0; i<=15;i++) {
+    //i%2==0   i переменная % подели на число следующее 2 (у любого четного числа остаток при деление равен 0)
+    if (i%2==0) {
+        continue;
+    }
+    System.out.println("Это не четное число."+i);
 }
+    }
+}
+
+
+//package Day3;
+//
+//public class break_continue {
+//    public static void main(String[] args) {
+//        int i = 0;
+//        while (true) {
+//            if (i == 15)
+//                // если if равен 15 то срабатывает  break;  и остонавливает
+//                break;
+//            System.out.println(i);
+//            i++;
+//
+//        }
+//        System.out.println("Мы вышли из цикла.");
+//    }
+//}
